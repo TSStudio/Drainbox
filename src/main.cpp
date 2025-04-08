8382c437fd9832c167c0b006d7b4f7512d747734
Mingyang Tong <TSS@mail.tmysam.top>
2025-04-08 12:02:54 +0800

    Add PID

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,28 @@
 #include "Adafruit_AHTX0.h"
 #include "Wire.h"
 #include "pin_defs.h"
+#include "PID_v1.h"
+#include "WiFi.h"
+#include "PubSubClient.h"
 
 TwoWire wire(0);
 
 Adafruit_AHTX0 ahtx0 = Adafruit_AHTX0();
 
 uint16_t pulse_counter = 0;
-uint8_t pwm_target_percentage = 0;
+double last_rpm=0;
 
-uint8_t fan_percentage_preset[4]= {25, 40, 20, 30};
+uint8_t target_fan_percentage = 64; // 0-255
+double target_fan_percentage_pid = 64; // 0-255
+double target_fan_rpm = 2220;
+
+PID fanPID(&last_rpm, &target_fan_percentage_pid, &target_fan_rpm, 0.06, 0.02, 0, DIRECT);
+
+typedef enum{
+    CONTROL_MODE_PERCENTAGE=0,
+    CONTROL_MODE_RPM=1,
+} fan_control_mode_t;
+fan_control_mode_t fan_control_mode = CONTROL_MODE_RPM;
 
 void interrupt_fan_pulse() {
     // Handle fan pulse interrupt here
@@ -28,7 +41,7 @@ void setup() {
     pinMode(FAN_PWM_PIN, OUTPUT);
     // set to 0
     // digitalWrite(FAN_PWM_PIN, LOW);
-    bool result=ledcAttach(FAN_PWM_PIN, 25000, 8); // Attach PWM to fan speed pin with 5kHz frequency and 8-bit resolution
+    bool result=ledcAttach(FAN_PWM_PIN, 25000, 8); // Attach PWM to fan speed pin with 25kHz frequency and 8-bit resolution
     if(!result){
         Serial.println("Failed to attach PWM to fan speed pin.");
     }
@@ -37,13 +50,13 @@ void setup() {
         Serial.println("Failed to set initial fan speed.");
     }
     Serial.println("Fan PWM initialized successfully.");
+    fanPID.SetMode(AUTOMATIC); // Set PID to automatic mode
 }   
 
 uint8_t loop_counter = 0;
 
 void loop() {
     uint16_t pcounter = pulse_counter;
-    Serial.printf("Loop counter: %d\n", loop_counter);
     if((loop_counter & 0x0f) == 0){
         // every 16 loops (16*1000ms = 16s)
         sensors_event_t humidity, temperature;
@@ -54,11 +67,29 @@ void loop() {
         Serial.print("Humidity: ");
         Serial.print(humidity.relative_humidity);
         Serial.println(" %");
-        ledcWrite(FAN_PWM_PIN, fan_percentage_preset[(loop_counter>>4) & 0x03]);
     }else{
         uint16_t rpm=pcounter*30;
+        last_rpm=static_cast<double>(rpm);
         Serial.print("RPM: ");
         Serial.println(rpm);
+        if(fan_control_mode == CONTROL_MODE_PERCENTAGE){
+            // set fan speed to target percentage
+            ledcWrite(FAN_PWM_PIN, target_fan_percentage);
+        }else if(fan_control_mode == CONTROL_MODE_RPM){
+            // set fan speed to target RPM using PID control
+            fanPID.Compute();
+            uint8_t output_percentage=0;
+            if(target_fan_percentage_pid > 255){
+                output_percentage = 255;
+            }else if(target_fan_percentage_pid < 0){
+                output_percentage = 0;
+            }else{
+                output_percentage = static_cast<uint8_t>(target_fan_percentage_pid);
+            }
+            Serial.print("Target fan percentage (PID): ");
+            Serial.println(output_percentage);
+            ledcWrite(FAN_PWM_PIN, output_percentage);
+        }
     }
     loop_counter++;
     pulse_counter = 0;
